93e9ef586538b2d5a4645188b41a86f06c1e84fd
Mariyam_Sardar <87272687+Mariyam97@users.noreply.github.com>
2022-09-27 22:42:58 +0530

    Super Department

---
+++ SuperDepartment.java
@@ -0,0 +1,23 @@
+package Services;
+
+public class SuperDepartment {
+	public String departmentName() {
+		String deptName = "Super Department";
+		return deptName;
+		}
+	
+	public String getTodaysWork() {
+		String getWork = "No Work as of now";
+		return getWork;
+		}
+	
+	public String getWorkDeadline() {
+		String getDeadline = "Nill";
+		return getDeadline;
+		}
+	
+	public String isTodayAHoliday() {
+		String isHoliday = "Today is not a holiday";
+		return isHoliday;
+		}
+}
